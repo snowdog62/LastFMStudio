a4414fa7823640036e5c98d2506fd1fbc5e36e8f
Mike Barber <mike@mbarber.com>
2015-08-28 18:47:01 +0200

    Corrected main form

--- LastFMStudio/LastFMStudio.Designer.cs
+++ LastFMStudio/LastFMStudio.Designer.cs
@@ -39,10 +39,6 @@
             this.albumBox = new System.Windows.Forms.TextBox();
             this.exportArtist = new System.Windows.Forms.Button();
             this.exportAlbum = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // userBox
@@ -142,42 +138,11 @@
             this.exportAlbum.UseVisualStyleBackColor = true;
             this.exportAlbum.Click += new System.EventHandler(this.exportAlbum_Click);
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(203, 135);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(186, 145);
-            this.tabControl1.TabIndex = 12;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(178, 119);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // LastFMStudio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 301);
-            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.exportAlbum);
             this.Controls.Add(this.exportArtist);
             this.Controls.Add(this.label3);
@@ -191,7 +156,6 @@
             this.Controls.Add(this.userBox);
             this.Name = "LastFMStudio";
             this.Text = "LastFMStudio";
-            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,9 +174,6 @@
         private System.Windows.Forms.TextBox albumBox;
         private System.Windows.Forms.Button exportArtist;
         private System.Windows.Forms.Button exportAlbum;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
